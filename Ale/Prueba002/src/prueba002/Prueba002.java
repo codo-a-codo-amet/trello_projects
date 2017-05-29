f23de440359515b1b94a68c603c7f9eb84282ff6
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-05-29 20:37:43 -0300

    Agregados 3 projectos enteros en netbeans de los ejemplos de la clase de hoy

---
+++ Ale/Prueba002/src/prueba002/Prueba002.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba002;
+
+/**
+ *
+ * @author alumno
+ */
+public class Prueba002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador = 0;
+        do {
+            System.out.println(contador);
+            contador = contador + 1;
+        } while(contador<100);
+    }
+    
+}
---
+++ Ale/Prueba003/src/prueba003/Prueba003.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba003;
+
+/**
+ *
+ * @author alumno
+ */
+public class Prueba003 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //   Inicializa la variable; como el while; 
+        for (int contador = 0; contador < 100; contador++) {
+            System.out.println(contador);
+        }
+    }
+    
+}
