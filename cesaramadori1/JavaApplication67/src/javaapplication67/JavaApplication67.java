b274d0c003cf3cb91d458994b5d48dab28524df0
DESKTOP-746IKV5\alumno <cesargamadori@gmail.com>
2017-06-05 21:31:26 -0300

    Actualizado

--- cesaramadori1/JavaApplication67/src/javaapplication67/JavaApplication67.java
+++ cesaramadori1/JavaApplication67/src/javaapplication67/JavaApplication67.java
@@ -13,15 +13,21 @@ public class JavaApplication67 {
         return 0;
     }
     public static void main(String[] args) {
-        Alumno unAlumno = new Alumno();
+        //Alumno unAlumno = new Alumno();
         
-        unAlumno.setNombre("Pepito");
-        System.out.println(unAlumno.dameTuNombre());
+        //unAlumno.setNombre("Pepito");
+        //System.out.println(unAlumno.dameTuNombre());
         
-        Perro unPerro = new Perro();
+        //Perro unPerro = new Perro();
         
-        System.out.println(unPerro.trotar());
-        System.out.println(unPerro.dameTuNombre());
+        //System.out.println(unPerro.trotar());
+        //System.out.println(unPerro.dameTuNombre());
+        Persona NN = new Persona();
+        System.out.println("Tu nombre y apellido es: "+ NN.getNombre()+" "+NN.getApellido());
+        System.out.println("Estas vestido con "+ NN.dequedoloresturopa());
+        
+        
+        
     }
     
 }
--- cesaramadori1/JavaApplication67/src/javaapplication67/Ropa.java
+++ cesaramadori1/JavaApplication67/src/javaapplication67/Ropa.java
@@ -10,64 +10,8 @@ package javaapplication67;
  * @author alumno
  */
 public class Ropa {
-    private String color;
-    private String talle;
-    private String tipo;
-    private String marca;
-
-    /**
-     * @return the color
-     */
-    public String getColor() {
-        return color;
-    }
-
-    /**
-     * @param color the color to set
-     */
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    /**
-     * @return the talle
-     */
-    public String getTalle() {
-        return talle;
-    }
-
-    /**
-     * @param talle the talle to set
-     */
-    public void setTalle(String talle) {
-        this.talle = talle;
-    }
-
-    /**
-     * @return the tipo
-     */
-    public String getTipo() {
-        return tipo;
-    }
-
-    /**
-     * @param tipo the tipo to set
-     */
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    /**
-     * @return the marca
-     */
-    public String getMarca() {
-        return marca;
-    }
-
-    /**
-     * @param marca the marca to set
-     */
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+    public String color;
+    public String talle;
+    public String tipo;
+    public String marca;
 }
