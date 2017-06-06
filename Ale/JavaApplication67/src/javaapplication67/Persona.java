76e293ddcfe91a77444e021ba8b93f179c8d9f68
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-06-05 21:29:33 -0300

    Agregado profesor y actualizado PErsona

--- Ale/JavaApplication67/src/javaapplication67/Persona.java
+++ Ale/JavaApplication67/src/javaapplication67/Persona.java
@@ -12,16 +12,43 @@ package javaapplication67;
 public class Persona implements IPersona{
     private String nombre;
     private String apellido;
+    private int edad;
     private Integer dni;
-    public String sexo;
-    public Ropa calzado;
-    public Ropa pantalones;
-    public Ropa sombrero;
+    private String sexo;
+    private Ropa calzado;
+    private Ropa pantalones;
+    private Ropa sombrero;
     
     protected String religion;
     
+    public Persona(){
+        nombre = "Sin nombre";
+        apellido = "Sin appelido";
+        edad = 18;
+        calzado = new Ropa();
+        calzado.color = "Negro";
+        calzado.marca = "Niquei";
+        calzado.talle = "40";
+    }
+    
+    public void ahoraTeLlamas(String nuevoNombre) {
+       nombre = nuevoNombre;
+    }
+    
+    public int queEdadTenes(){
+        return edad;
+    }
+    
+    public String deQueColorEsTuRopa(){
+        return "el panatlon es " +  pantalones.color + " y el sombrero es de color " + sombrero.color;
+    }
+    
     public String comoTeLlamas() {
-        return nombre;
+         if ( queEdadTenes() < 18) {
+             return "Ni loco te digo, chau!";
+         } else {
+             return nombre;
+         }
     }
    
 }
