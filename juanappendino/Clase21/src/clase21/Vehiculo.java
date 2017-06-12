fce8bf0de414cf85dfe468c9d1856ae1f7e8a18a
cerebro098 <grupobairesdigital@gmail.com>
2017-06-12 20:14:30 -0300

    tratando de subir algo

---
+++ juanappendino/Clase21/src/clase21/Vehiculo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase21;
+
+/**
+ *
+ * @author alumno
+ */
+public class Vehiculo implements IVehiculo {
+    
+    protected Integer pasajeros;
+    protected Integer ruedas;
+    private String tipoMotor;
+    private String combustible;
+    protected String marca;
+    private String modelo;
+    private Integer año;
+    private String patente;
+    
+    public Vehiculo(){
+        
+    }
+    
+    public boolean Frenar(){
+        return true;
+    }
+            
+}
