542f7617b955fcd1693b08912c7f005d4657a7c1
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-06-21 21:15:06 -0300

    numero ger

---
+++ Ale/Clase25/src/clase25/Numero_ger.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase25;
+
+/**
+ *
+ * @author alumno
+ */
+public class Numero_ger implements IAritmetica{
+    
+        private int unNumeroEntero;
+        
+        public Numero_ger (int valor){
+        unNumeroEntero = valor;
+        }
+            
+    public void Sumar(IAritmetica toAdd) {
+
+    }
+
+    public void Restar(IAritmetica toAdd) {
+
+    }
+
+    public void Multiplicar(IAritmetica toAdd) {
+
+    }
+
+    public void Dividir(IAritmetica toAdd) {
+
+    }
+
+    public Boolean EsIgualA(IAritmetica toAdd) {
+           return Boolean.FALSE;
+    }
+    
+    public String descripcion(){
+        return "";
+    }
+    
+    
+    
+}
