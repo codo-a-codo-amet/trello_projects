df1d1d3084b85903a38024fa7cdc149c833348a6
Rodolfo cerebro098 <grupobairesdigital@gmail.com>
2017-06-26 20:21:26 -0300

    CLASE27CINECEREBRO

---
+++ cerebrojava/Clase25/src/clase25/Numero.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase25;
+
+/**
+ *
+ * @author alumno
+ */
+public class Numero {
+    
+    public void Sumar(IAritmetica toAdd){
+        
+    }
+    public void Restar(IAritmetica toAdd){
+        
+    }
+    public void Multiplicar(IAritmetica toAdd){
+        
+    }
+    public void Dividir(IAritmetica toAdd){
+        
+    }
+    public void EsIgualA(IAritmetica toAdd){
+        
+    }   
+    
+    public void descripcion(){
+        
+    }    
+}
