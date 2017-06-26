20af52ac4652bb931a84347653e68e5fd42feab9
GermanProyecto <german_uba_ch@hotmail.com>
2017-06-26 20:22:06 -0300

    subiendo!

---
+++ GerZapp/Clase27/src/clase27/Clase27.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase27;
+
+/**
+ *
+ * @author alumno
+ */
+public class Clase27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+         cine.cantidad_butacas = 180
+         cine.cantidad_salas = 9        
+    
+
+    }
+    
+    
+}
