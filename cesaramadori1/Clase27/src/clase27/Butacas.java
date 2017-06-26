e52f67e6c0a3dce47a8a436d8fd02bba5e4104c0
Cesar <cesargamadori@gmail.com>
2017-06-26 20:27:22 -0300

    Sala de cine

---
+++ cesaramadori1/Clase27/src/clase27/Butacas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase27;
+
+/**
+ *
+ * @author alumno
+ */
+public class Butacas {
+    private final int filas = 13;
+    private final int asientosPorFila = 20;
+}
---
+++ cesaramadori1/Clase27/src/clase27/Sala.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alumno
+ */
+public class Sala extends Complejo {
+    Pelicula peli1 = new Pelicula("E.T., el extraterrestre (1982)", "Ciencia Ficción", 115) ;
+    List <Butacas> asientos = new ArrayList();
+}
