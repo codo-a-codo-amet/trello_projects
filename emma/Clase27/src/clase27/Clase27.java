b62e161a07b8201621af661e4cee6baa504242d5
Emma <emanueljeremiascaceres@gmail.com>
2017-06-26 20:54:19 -0300

    Village App

--- emma/Clase27/src/clase27/Clase27.java
+++ emma/Clase27/src/clase27/Clase27.java
@@ -16,6 +16,20 @@ public class Clase27 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    
+    
+  cine unCine = new cine();
+        
+            
+        System.out.println(unCine.dameelcomplejo());
+        System.out.println(unCine.damelapelicula());
+        System.out.println(unCine.dameloshorario());
+        System.out.println(unCine.damelassala());
+        System.out.println(unCine.damelasbutacas());
+                
+         cine unComplejo = new complejo ();
+         System.out.println(unComplejo.damelapelicula());
+         
+         }
     
 }
