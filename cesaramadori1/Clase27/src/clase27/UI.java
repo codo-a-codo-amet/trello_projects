db11a7d71b498e81a38c05a0c7010c99f459d368
Cesar Amadori <cesargamadori@gmail.com>
2017-07-01 23:09:30 -0300

    buscador por pelicula y por sala

--- cesaramadori1/Clase27/src/clase27/UI.java
+++ cesaramadori1/Clase27/src/clase27/UI.java
@@ -21,19 +21,25 @@ public class UI extends javax.swing.JFrame  {
     Complejo Hoyts = new Complejo();
     public UI() {
         initComponents();
-        
+        //Asignar la lista de salas al combo box
         comboSalas.setModel(new DefaultComboBoxModel(Hoyts.salas.toArray()));
+        //Instanciar una sala para asignarle la que esta seleccionada en el combo box
         Sala salaSeleccionada = (Sala)comboSalas.getSelectedItem();
+        //Llamado al metodo para mostar en txtPelicula, segun sala seleccionada
         obtenerPelicula(salaSeleccionada);
-        
-        
-        butacasVacias(salaSeleccionada);
-         
+        //Llamado al metodo que muestra las butacas vacias de la sala seleccionada
+        //butacasVacias(salaSeleccionada);
+        //Asignar la lista de peliculas al combo box para seleccionar por pelicula la sala
+        comboPeliculas.setModel(new DefaultComboBoxModel(Hoyts.listado_de_Peliculas));
+        salaporPelicula();
         
     }
     
     public void obtenerPelicula(Sala laSala){
-        txtPelicula.setText(laSala.laPelicula.getNombre());  
+        
+        txtPelicula.setText(laSala.laPelicula.getNombre()); 
+        butacasVacias(laSala);
+        butacasOcupadas(laSala);
     }
     
     public void butacasVacias (Sala laSala){
@@ -61,7 +67,13 @@ public class UI extends javax.swing.JFrame  {
         String cantidad = String.valueOf(listOcupadas.size());
         jtxttotalvendidas.setText(cantidad);
     }
-    
+    public void salaporPelicula (){
+        for (int i = 0; i < Hoyts.salas.size(); i++) {
+                if (Hoyts.salas.get(i).laPelicula.getNombre() == (String) comboPeliculas.getSelectedItem()) {
+                    jtxtSala.setText(Hoyts.salas.get(i).nombre);
+                }
+            }
+    }
     public void comprarentradas(){
         Sala salaSeleccionada = (Sala)comboSalas.getSelectedItem();
         int item = this.jButlibres.getSelectedIndex();
@@ -76,6 +88,7 @@ public class UI extends javax.swing.JFrame  {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jtxtSala = new javax.swing.JTextField();
         comboSalas = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         jButlibres = new javax.swing.JList<>();
@@ -90,8 +103,25 @@ public class UI extends javax.swing.JFrame  {
         jtxttotallibres = new javax.swing.JTextField();
         jtxttotalvendidas = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        comboPeliculas = new javax.swing.JComboBox<>();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jtxtSala.setEditable(false);
+        jtxtSala.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtxtSalaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jtxtSala, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 10, 160, 30));
 
         comboSalas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         comboSalas.addItemListener(new java.awt.event.ItemListener() {
@@ -109,6 +139,7 @@ public class UI extends javax.swing.JFrame  {
                 comboSalasPropertyChange(evt);
             }
         });
+        getContentPane().add(comboSalas, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 60, 110, 30));
 
         jButlibres.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -126,6 +157,8 @@ public class UI extends javax.swing.JFrame  {
             }
         });
         jScrollPane1.setViewportView(jButlibres);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, 93, 230));
 
         txtPelicula.setEditable(false);
         txtPelicula.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
@@ -149,10 +182,13 @@ public class UI extends javax.swing.JFrame  {
                 txtPeliculaActionPerformed(evt);
             }
         });
+        getContentPane().add(txtPelicula, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 60, 332, 30));
 
         jLabel1.setText("Sala");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 28, -1));
 
         jLabel2.setText("Pelicula");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 70, -1, -1));
 
         jButocupadas.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -161,28 +197,37 @@ public class UI extends javax.swing.JFrame  {
         });
         jScrollPane2.setViewportView(jButocupadas);
 
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 170, 93, 230));
+
         btnComprarEntrada.setText("Comprar Entrada");
         btnComprarEntrada.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnComprarEntradaActionPerformed(evt);
             }
         });
+        getContentPane().add(btnComprarEntrada, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 300, -1, -1));
 
         jLabel3.setText("Butacas Libres");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 100, -1));
 
         jLabel4.setText("Butacas Vendidas");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 110, 120, -1));
 
+        jtxttotallibres.setEditable(false);
         jtxttotallibres.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jtxttotallibresActionPerformed(evt);
             }
         });
+        getContentPane().add(jtxttotallibres, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 100, 93, 30));
 
+        jtxttotalvendidas.setEditable(false);
         jtxttotalvendidas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jtxttotalvendidasActionPerformed(evt);
             }
         });
+        getContentPane().add(jtxttotalvendidas, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 100, 93, 30));
 
         jButton1.setText("Anular Venta");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -190,71 +235,34 @@ public class UI extends javax.swing.JFrame  {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 300, 110, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(comboSalas, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jLabel2))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(jtxttotallibres, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addComponent(btnComprarEntrada)))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtPelicula)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                                .addComponent(jtxttotalvendidas, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)))
-                        .addGap(0, 74, Short.MAX_VALUE)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(comboSalas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jtxttotalvendidas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jtxttotallibres, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnComprarEntrada)
-                    .addComponent(jButton1))
-                .addContainerGap(19, Short.MAX_VALUE))
-        );
+        jLabel5.setText("Pelicula");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
+
+        jLabel6.setText("Listado Vendidas");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 140, -1, 20));
+
+        jLabel7.setText("Listado Libres");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, -1, 20));
+
+        comboPeliculas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        comboPeliculas.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                comboPeliculasItemStateChanged(evt);
+            }
+        });
+        getContentPane().add(comboPeliculas, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 250, 30));
+
+        jLabel8.setText("Sala");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 20, -1, -1));
+
+        jLabel9.setText("Buscar Sala por Pelicula");
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 20, -1, -1));
+
+        jLabel10.setText("Buscar Pelicula por Sala");
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 70, -1, -1));
+        getContentPane().add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 780, 20));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -293,10 +301,7 @@ public class UI extends javax.swing.JFrame  {
         Sala salaSeleccionada = (Sala)comboSalas.getSelectedItem();
         if (evt.getStateChange() == ItemEvent.SELECTED) {
            
-                this.txtPelicula.setText(salaSeleccionada.laPelicula.getNombre());
-                butacasVacias((Sala)comboSalas.getSelectedItem());
-                butacasOcupadas((Sala)comboSalas.getSelectedItem());
-            
+            obtenerPelicula(salaSeleccionada);
         }
     }//GEN-LAST:event_comboSalasItemStateChanged
 
@@ -329,6 +334,19 @@ public class UI extends javax.swing.JFrame  {
        butacasVacias((Sala)comboSalas.getSelectedItem());
        butacasOcupadas((Sala)comboSalas.getSelectedItem());// TODO add your
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jtxtSalaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtxtSalaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtxtSalaActionPerformed
+
+    private void comboPeliculasItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_comboPeliculasItemStateChanged
+        
+        
+        if (evt.getStateChange() == ItemEvent.SELECTED) {
+            
+            salaporPelicula();
+        }
+    }//GEN-LAST:event_comboPeliculasItemStateChanged
 
     /**
      * @param args the command line arguments
@@ -367,16 +385,25 @@ public class UI extends javax.swing.JFrame  {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnComprarEntrada;
+    private javax.swing.JComboBox<String> comboPeliculas;
     private javax.swing.JComboBox<String> comboSalas;
     private javax.swing.JList<String> jButlibres;
     private javax.swing.JList<String> jButocupadas;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextField jtxtSala;
     private javax.swing.JTextField jtxttotallibres;
     private javax.swing.JTextField jtxttotalvendidas;
     private javax.swing.JTextField txtPelicula;
