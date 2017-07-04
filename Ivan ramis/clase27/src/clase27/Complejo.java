813030295c7c2c7aa4ed026bab9c9852e0a237bb
ivan ramis <ivanra48@gmail.com>
2017-07-03 21:21:01 -0300

    no message

--- Ivan ramis/clase27/src/clase27/Complejo.java
+++ Ivan ramis/clase27/src/clase27/Complejo.java
@@ -52,6 +52,7 @@ public class Complejo implements IComplejo {
             if (unaButaca.ocupada) {
                 System.out.println("EstaOcupada");
             } else {
+                lista_de_butacas_libres.add(unaButaca);
                 System.out.println("Esta libre");
             }
         }
