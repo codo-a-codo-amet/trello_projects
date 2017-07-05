e3287e901a6e0301b586032fc5ffbf1b3fcdb7cd
Walter Ceballo <waceballo@gmail.com>
2017-07-05 18:48:03 -0300

    Mas sobre la clase 27

---
+++ waceballo/ComplejoCine/src/complejocine/Pelicula.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complejocine;
+
+import java.util.*;
+
+/**
+ *
+ * @author walter
+ */
+public class Pelicula {
+    
+    private Sala sala;
+
+    public Pelicula(){
+        
+        sala = new Sala();
+        
+
+    }
+
+    public Sala getSala(){
+        return sala;
+    }
+    
+
+    
+    
+    
+}
