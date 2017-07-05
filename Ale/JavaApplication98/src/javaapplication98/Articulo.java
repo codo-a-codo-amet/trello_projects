d9d28484fef24546453e109c6c185e49bc888cfd
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-07-05 19:21:39 -0300

    conflict after merge

---
+++ Ale/JavaApplication98/src/javaapplication98/Articulo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication98;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Articulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=2, y=6;
+        if((x<y && x!=y) || !(x==y))
+        System.out.println("Verdadeor");
+        else
+        System.out.println("Falso");
+        
+        }
+      
+    
+    }
