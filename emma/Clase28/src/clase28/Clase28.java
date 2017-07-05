599a4ca5d1a6f662e43b8faf96782cc049404167
Emma <emanueljeremiascaceres@gmail.com>
2017-07-05 20:15:23 -0300

    clase28

---
+++ emma/Clase28/src/clase28/Clase28.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase28;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Clase28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Ingrese la cantidad personas");
+        Scanner unScanner = new Scanner(System.in);
+        int cantidad_de_personas = unScanner.nextInt();
+        
+        List<Persona> una_lista_de_personas = new ArrayList();
+        
+        for (int i = 0; i < cantidad_de_personas; i++) {
+            
+            System.out.println ("Ingrese el nombre de la persona");
+            String unNombre = unScanner.next();
+            
+            System.out.println ("Ingrese el apellido de la persona");
+            String unApellio = unScanner.next();
+            
+            una_lista_de_personas.add(new Persona());
+        }
+        
+    }
+    
+}
