b10ae41a874feec84735f5bea5d577bd5203af16
DESKTOP-H96BG9A\alumno <waceballo@gmail.com>
2017-08-02 20:04:20 -0300

    Patron de Diseño Singleton...

--- waceballo/Clase40/src/clase40/Clase40.java
+++ waceballo/Clase40/src/clase40/Clase40.java
@@ -17,25 +17,34 @@ public class Clase40 {
     public static void main(String[] args) {
         // TODO code application logic here
         Microfono unMicrofono = Microfono.getInstance();
-        Microfono otroMicrofono = Microfono.getInstance();        
+        Microfono otroMicrofono = Microfono.getInstance();
         
-        //System.out.println(unMicrofono);
-        //System.out.println(otroMicrofono);
-        Boolean er = Boolean.FALSE; 
-        
-        try {
-            System.out.println(10/0);
-        } catch (Exception e) {
-            er = Boolean.TRUE;
-            
-            if (er){
-                System.out.println("No se puede dividir por cero. "+e.getMessage());
-            }
-
-        }
+        System.out.println(unMicrofono);
+        System.out.println(otroMicrofono);
+               
+//        Boolean er = Boolean.FALSE; 
+//        
+//        try {
+//            System.out.println(10/0);
+//        } catch (Exception e) {
+//            er = Boolean.TRUE;
+//            
+//            if (er){
+//                System.out.printl n("No se puede dividir por cero. "+e.getMessage());
+//            }
+//
+//        }
         
         //SingletonGeneric<Parlante> unParlante = SingletonGeneric.getInstance();
         //System.out.println(unParlante);
+        
+        ConexionBD con = ConexionBD.getInstance();
+        ConexionBD con1 = ConexionBD.getInstance();
+        ConexionBD con2 = ConexionBD.getInstance();
+        
+        System.out.println(con);
+        System.out.println(con1);
+        System.out.println(con2);
         
     }
     
