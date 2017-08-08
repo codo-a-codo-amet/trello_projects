65978440ef50cbe6e3d7583ee5ab59a6582d2a8f
Cesar Amadori <cesargamadori@gmail.com>
2017-08-08 13:39:08 -0300

    Ajustando Vista ahorcado

--- Ahorcado en el fondo/Ahorcado/src/ahorcado/Ahorcado.java
+++ Ahorcado en el fondo/Ahorcado/src/ahorcado/Ahorcado.java
@@ -5,6 +5,8 @@
  */
 package ahorcado;
 
+import ahorcado.Vista.Vista;
+
 /**
  *
  * @author alumno
@@ -15,7 +17,8 @@ public class Ahorcado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vista unicavista = new Vista();
+        unicavista.setVisible(true);
     }
     
 }
