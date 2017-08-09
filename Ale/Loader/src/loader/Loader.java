8ffd56c717746f6d011343a6c2a27c4cb8b774ee
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-08-09 20:45:22 -0300

    Loader para cargar archivo de "Ahorcado.txt"

--- Ale/Loader/src/loader/Loader.java
+++ Ale/Loader/src/loader/Loader.java
@@ -5,8 +5,12 @@
  */
 package loader;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -17,16 +21,33 @@ public class Loader {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         // TODO code application logic here
+        final String fullPath = "ahorcado.txt";
+        boolean loadInternatl = true;
+        BufferedReader buffer = null;
         try {
-            FileReader reader = new FileReader("ahorcado.txt");
-        }
-        catch(FileNotFoundException e){
+
+            if (loadInternatl) {
+                InputStream is = Loader.class.getResourceAsStream("ahorcado.txt");
+                buffer = new BufferedReader(new InputStreamReader(is));
+            } else {
+                FileReader reader = new FileReader(fullPath);
+                buffer = new BufferedReader(reader);
+            }
+
+            String linea;
+
+            while ((linea = buffer.readLine()) != null) {
+                System.out.println(linea);
+
+            }
+        } catch (FileNotFoundException e) {
             System.out.println("Ups File not found");
+        } catch (IOException ioE) {
+            System.out.println("Ups IO error");
         }
-        
-        
+
     }
-    
+
 }
