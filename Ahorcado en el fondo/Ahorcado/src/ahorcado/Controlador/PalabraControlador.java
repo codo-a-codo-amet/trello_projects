6080be6489f8cba3dbc6a51b0d89ce331151cbaf
RoCab <rj-cab@hotmail.com>
2017-08-09 21:51:06 -0300

    palabra aleatoria

--- Ahorcado en el fondo/Ahorcado/src/ahorcado/Controlador/PalabraControlador.java
+++ Ahorcado en el fondo/Ahorcado/src/ahorcado/Controlador/PalabraControlador.java
@@ -14,19 +14,36 @@ import java.util.*;
  */
 public class PalabraControlador implements IPalabraControlador {
 
-    private List<Palabra> listadoPalabras;
-    private String[] palabritas= {"Estancia","desarrollo","alquiler","Dueño","Lider","Ingenio","insecto","politica","ajedrez","aprobar"};
-    private List<String> letrasUsadas;
-    public PalabraControlador (){
+    private final List<Palabra> listadoPalabras;
+    private final String[] palabritas= {"Estancia","desarrollo","alquiler","Dueño","Lider","Ingenio","insecto","politica","ajedrez","aprobar"};
+    private final List<String> letrasUsadas;
+    
+    public PalabraControlador (String string){
         listadoPalabras = new ArrayList();
         letrasUsadas = new ArrayList();
-        for (int i = 0; i < palabritas.length; i++) {
-            listadoPalabras.add(new Palabra(palabritas[i],false));
+        
+        
+        for (String palabrita : palabritas) {
+            listadoPalabras.add(new Palabra(palabrita, false));
+    
+         //   public void PalabraSeleccionada;{
+        
+         Random azar = new Random();
+        int dato1;
+        dato1 = azar.nextInt(palabrita.length());
+        int inicio = palabrita.indexOf(" ");
+        int fin = palabrita.indexOf(" ",inicio + dato1);
+        string = palabrita.substring(inicio + dato1, fin);
+        System.out.println(string);
+
         }
     }
+        
     
-    
-    
+
+    public PalabraControlador() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     @Override
     public void verificarLetra(String letra) {
@@ -39,4 +56,16 @@ public class PalabraControlador implements IPalabraControlador {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+
+    /* if (lista.indexOf(cadena) != -1){
+      for (t=0;t< inicio && t>fin; t++){
+       l=lista.indexOf(cadena.charAt(t));
+       y=lista.charAt(l);
+       System.out.println(l);
+       System.out.println(y);
+      }
+      
+     }*/
 }
+
+    
