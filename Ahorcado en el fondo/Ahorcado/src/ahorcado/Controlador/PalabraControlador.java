e3e721570777f658583fd0b8c1bc32c75dbfa762
Cesar Amadori <cesargamadori@gmail.com>
2017-08-10 20:30:29 -0300

    Casi completo

--- Ahorcado en el fondo/Ahorcado/src/ahorcado/Controlador/PalabraControlador.java
+++ Ahorcado en el fondo/Ahorcado/src/ahorcado/Controlador/PalabraControlador.java
@@ -15,66 +15,105 @@ import java.util.*;
 public class PalabraControlador implements IPalabraControlador {
 
     private List<Palabra> listadoPalabras;
-    private String[] palabritas= {"Estancia","desarrollo","alquiler","Dueño","Lider","Ingenio","insecto","politica","ajedrez","aprobar"};
+    private String[] palabritas = {"Estancia", "desarrollo", "alquiler", "Dueño", "Lider", "Ingenio", "insecto", "politica", "ajedrez", "aprobar"};
     private List<String> letrasUsadas;
-    
-    public PalabraControlador (){
+    private int juego = 1;
+    private boolean estaLaLetra = false;
+    private int totalLetras = 0;
+    private String palabrajuego;
+    String[] laPalabraArray;
+    private int contadorLetras = 0;
+
+    public PalabraControlador() {
         listadoPalabras = new ArrayList();
         letrasUsadas = new ArrayList();
         for (int i = 0; i < palabritas.length; i++) {
-            listadoPalabras.add(new Palabra(palabritas[i],false));
+            listadoPalabras.add(new Palabra(palabritas[i], false));
         }
+        palabrajuego = listadoPalabras.get(1).getPalabra();
+        laPalabraArray = caracterPalabra(palabrajuego);
     }
-    
-    
-    
-    
-    @Override
-    public void verificarLetra(String letra) {
-        letrasUsadas.add(letra);
-        System.out.println("Verificando la letra "+ letra);
+
+    public void resetearJuego() {
+        juego = 1;
+        estaLaLetra = false;
+        totalLetras = 0;
+        contadorLetras = 0;
+    }
+
+    public boolean ganaste() {
+        boolean ganaste = false;
+        if (contadorLetras == laPalabraArray.length) {
+            ganaste = true;
+            System.out.println("GANASTE!!!!");
+        }
+        return ganaste;
     }
 
     @Override
-    public String  entregarGuiones() {
-        String laPalabra = listadoPalabras.get(0).getPalabra();
-        String[] laPlabraArray = caracterPalabra(laPalabra);
-        String[] laPalabraArrayGuion = verificarEstaPalabra(laPlabraArray);
-       
-        String guiones=Arrays.toString(laPalabraArrayGuion).replaceAll("\\[|\\]|,|", "");
+    public void verificarLetra(String letra) {
+        letrasUsadas.add(letra);
+        for (int i = 0; i < laPalabraArray.length; i++) {
+            if (laPalabraArray[i].equals(letra)) {
+                estaLaLetra = true;
+                System.out.println("Esta la letra " + letra);
+                contadorLetras++;
+            }
+        }
+        if (estaLaLetra == false) {
+            juego++;
+            System.out.println("Juego " + juego);
+        }
+        estaLaLetra = false;
+    }
+
+    public String entregarGuionesPrimeraVez() {
+        String[] palabraVerificada = new String[laPalabraArray.length];
+        for (int j = 0; j < laPalabraArray.length; j++) {
+            palabraVerificada[j] = "_ ";
+        }
+        String guiones = Arrays.toString(palabraVerificada).replaceAll("\\[|\\]|,|", "");
         return guiones;
     }
-    
-    public String[] verificarEstaPalabra(String[] palabrita){
+
+    @Override
+    public String entregarGuiones() {
+        String[] laPalabraArrayGuion = verificarEstaPalabra(laPalabraArray);
+        String guiones = Arrays.toString(laPalabraArrayGuion).replaceAll("\\[|\\]|,|", "");
+        return guiones;
+    }
+
+    public String[] verificarEstaPalabra(String[] palabrita) {
         String[] palabraVerificada = new String[palabrita.length];
         for (int i = 0; i < letrasUsadas.size(); i++) {
+            totalLetras = letrasUsadas.size() * palabrita.length;
             for (int j = 0; j < palabrita.length; j++) {
-                if (letrasUsadas.get(i).equals(palabrita[j]) ) {
-                    palabraVerificada[j]=letrasUsadas.get(i);
-                    System.out.println("La palabra contiene "+letrasUsadas.get(i) );
-                }else{
-                    if ((palabraVerificada[j]==null) || (palabraVerificada[j]=="_ ")) {
-                        palabraVerificada[j]="_ ";
-                        System.out.println("La palabra no contiene "+letrasUsadas.get(i));
+                if (letrasUsadas.get(i).equals(palabrita[j])) {
+                    palabraVerificada[j] = letrasUsadas.get(i);
+                } else {
+                    if ((palabraVerificada[j] == null) || (palabraVerificada[j] == "_ ")) {
+                        palabraVerificada[j] = "_ ";
                     }
                 }
             }
         }
-        
-        
         return palabraVerificada;
     }
 
     @Override
     public String[] caracterPalabra(String palabra) {
-      String [] laLetra;
-        laLetra = new String [palabra.length()];
+        String[] laLetra;
+        laLetra = new String[palabra.length()];
         for (int i = 0; i < palabra.length(); i++) {
-            String soloLetra = palabra.substring(i, i+1);
+            String soloLetra = palabra.substring(i, i + 1);
             laLetra[i] = soloLetra.toUpperCase();
-            
-            
+
         }
-        return laLetra; 
+        return laLetra;
     }
+
+    public int getJuego() {
+        return juego;
+    }
+
 }
