f65a42460fa67cf0242fc6c7e0ba395740408e9e
Geer <German_uba_ch@hotmail.com>
2017-08-14 19:21:39 -0300

    actualizando...

--- Los del diome/ahorcado/src/ahorcado/Ahorcado.java
+++ Los del diome/ahorcado/src/ahorcado/Ahorcado.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ahorcado;
 
-/**
- *
- * @author alumno
- */
 public class Ahorcado {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Tablero tablero = new Tablero();
+        tablero.imprimir();
+        Jugador jugador = new Jugador();
+        boolean ganador = false;
+
+        for (int i = 1; i <= tablero.getCantidadElementos() * 2; i++) {
+            String letra = jugador.pedirLetra();
+            tablero.verificar(letra);
+            tablero.imprimir();
+            if (tablero.verificarGanador()) {
+                ganador = true;
+                 break;
+            }
+        }
+        if (ganador) {
+            System.out.println("HAZ GANADO!!!");
+            
+        }else{
+            System.out.println("VUELVE A INTENTARLO!!!");
+        }
     }
-    
+
 }
+
+                
+                
