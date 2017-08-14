13327516a9f6e3f1213542f240cb719736f50b60
Alejandro <alejandro.medici@nextiteration.com.ar>
2017-08-14 19:28:12 -0300

    Codigo saraza

--- Ale/Consumo/src/consumo/Consumo.java
+++ Ale/Consumo/src/consumo/Consumo.java
@@ -3,17 +3,18 @@ package consumo;
 public class Consumo {
 
     public static void main(String[] args) {
-        AparatoElectrico bombilla = new AparatoElectrico(150);
+            //Crear objetos de la clase AparatoElectrico
+        AparatoElectrico bombilla = new AparatoElectrico(150);  
         AparatoElectrico plancha = new AparatoElectrico(2000);
 
-        AparatoElectrico.imprimir(); //Muestra el consumo total
+        AparatoElectrico.imprimirConsumo(); //Muestra el consumo total
         bombilla.enciende(); //Enciende la bombilla
         plancha.enciende(); //Enciende la plancha
 
-        AparatoElectrico.imprimir();//Muestra el consumo total
+        AparatoElectrico.imprimirConsumo();//Muestra el consumo total
         plancha.apaga(); //Apaga la plancha
 
-        AparatoElectrico.imprimir(); //Muestra el consumo total
+        AparatoElectrico.imprimirConsumo(); //Muestra el consumo total
 
     }
 }
