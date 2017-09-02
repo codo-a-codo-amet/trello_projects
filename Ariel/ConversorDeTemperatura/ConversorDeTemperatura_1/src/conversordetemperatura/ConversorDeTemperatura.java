97b363e434099b28c202c24cb1a6129cf127e176
Reimon\Li <arielrs35gmail.com>
2017-09-02 20:32:40 -0300

    Conversor de temperatura

---
+++ Ariel/ConversorDeTemperatura/ConversorDeTemperatura_1/src/conversordetemperatura/ConversorDeTemperatura.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversordetemperatura;
+
+/**
+ *
+ * @author alumno
+ */
+public class ConversorDeTemperatura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Conversor miTemperatura = new Conversor();
+        VistaConversor vista = new VistaConversor();
+        vista.setVisible(true);
+        
+      
+        
+        
+    }
+    
+}
