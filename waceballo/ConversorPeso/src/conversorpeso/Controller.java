e6f67dad59a9c1010df86baeae9adfec9f378a9a
Walter Ceballo <waceballo@gmail.com>
2017-09-06 21:04:40 -0300

    Conversor de Peso

---
+++ waceballo/ConversorPeso/src/conversorpeso/Controller.java
@@ -0,0 +1,60 @@
+package conversorpeso;
+
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author alumno
+ */
+public class Controller implements IViewEventListener {
+    Principal unaVista;
+    ConversorPeso con;
+
+    public Controller() {
+        unaVista = new Principal();
+        con = new ConversorPeso();
+    }
+
+    public void Run() {
+        //A partir de ahora, escucha lo que le sucede a la vista.
+        unaVista.AddEventListener(this);
+
+        unaVista.setVisible(true);
+    }
+
+    @Override
+    public void listen(Event event) {
+        ActionEvent ae = (ActionEvent) event.target;
+
+        if (ae.getActionCommand().equalsIgnoreCase("Salir")) {
+            System.exit(0);
+        }
+
+        Integer unidadAConvertir = unaVista.getCbDesdeUnidad().getSelectedIndex();
+
+        if (unidadAConvertir == 0) {
+            JOptionPane.showMessageDialog(unaVista, "Debe seleccionar una opcion");
+        } else {
+            
+            Double valorAconvertir = Double.parseDouble(unaVista.getTxtValorAConvertir().getText().replace(",", "."));
+            Double v = con.ConvertirDistancia(unidadAConvertir, valorAconvertir);
+            //System.out.println("valor convertido "+v);
+            DecimalFormat df = new DecimalFormat("######.#####");
+
+            unaVista.getTxtValorConvertido().setText(df.format(v));
+            //unaVista.getTxtValorConvertido().setText(v.toString());
+        }
+
+    }
+
+    
+    
+}
