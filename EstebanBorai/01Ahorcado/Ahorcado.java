41562294f9eef982501a32d8c88ca8db0cfed81e
Esteban Borai <leonardoestebanbba@gmail.com>
2017-09-23 15:49:38 -0300

    Update Ahorcado.java

--- EstebanBorai/01Ahorcado/Ahorcado.java
+++ EstebanBorai/01Ahorcado/Ahorcado.java
@@ -7,31 +7,38 @@ public class Ahorcado {
     
     private String palabra;
     private char [] tabla;
+    private int oportunidades;
     Scanner input = new Scanner(System.in);
     
     public Ahorcado (String palabra){
         this.palabra = palabra;
+        this.oportunidades = 6;
     }
     
     public void jugar(){
         setting();
-        for (int i = 1; i <= 6; i++){
+        do {
             
             System.out.print("Oportunidades: ");
-            for (int x = 6; x > 0; x--){
+            for (int x = this.oportunidades; x > 0; x--){
                 System.out.print("♥ ");
             }
             
             System.out.println("");
             
-            if (i == 1){
-                mostrarTabla();
-            }
-            
             System.out.println("Adivina la palabra: ");
             String lt = input.next();
             adivina(lt.charAt(0));
-        }
+            
+            if (victoria()){
+                System.out.println("Has ganado!");
+                break;
+            }
+            
+            if (this.oportunidades == 0){
+                System.out.println("Has perdido!");
+            }
+        } while (this.oportunidades != 0);
     }
     
     private void setting() {
@@ -56,6 +63,7 @@ public class Ahorcado {
         if (resolvio == false) {
             System.out.printf("Letra %c no existe en la palabra\n",
                     a);
+            this.oportunidades--;
         }
         
         mostrarTabla();
@@ -66,6 +74,17 @@ public class Ahorcado {
             System.out.print(this.tabla[i] + " ");
         }
         System.out.println("");
+    }
+
+    private boolean victoria() {
+        boolean split = true;
+        for (int i = 0; i < this.tabla.length; i++){
+            if (this.tabla[i] == '_'){
+                split = false;
+                break;
+            }
+        }
+        return split;
     }
     
 }
